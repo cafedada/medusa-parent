d6d5388b9ea2310068665f0ab0f23334150b1f38
cafedada <cafedada@vilya.io>
2019-12-04 07:53:46 +0800

    Update MyBatisModule.java

--- medusa-jdbc/src/main/java/io/vilya/server/medusa/jdbc/MyBatisModule.java
+++ medusa-jdbc/src/main/java/io/vilya/server/medusa/jdbc/MyBatisModule.java
@@ -28,74 +28,76 @@ import io.vilya.server.medusa.jdbc.demo.LogMapper;
  */
 public class MyBatisModule extends AbstractModule {
 
-	private DataSource dataSource;
-	
-	private TransactionFactory transactionFactory;
-	
-	@Override
-	protected void configure() {
-		HikariDataSource dataSource = getDataSource();
-		Configuration configuration = createMyBatisConfiguration(dataSource);
-		scanXmlMappers(configuration);
+    private DataSource dataSource;
 
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+    private TransactionFactory transactionFactory;
 
-		for (Class<?> mapperInterface : configuration.getMapperRegistry().getMappers()) {
-			bind((Class<Object>) mapperInterface).toInstance(Reflection.newProxy(mapperInterface,
-					new MapperInterceptor(sqlSessionFactory, mapperInterface)));
-		}
-		
-	}
+    @Override
+    protected void configure() {
+        HikariDataSource dataSource = getDataSource();
+        Configuration configuration = createMyBatisConfiguration(dataSource);
+        scanXmlMappers(configuration);
 
-	private Configuration createMyBatisConfiguration(DataSource dataSource) {
-		Environment environment = new Environment("dev", new JdbcTransactionFactory(), dataSource);
-		Configuration configuration = new Configuration(environment);
-		configuration.setMapUnderscoreToCamelCase(true);
-		return configuration;
-	}
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
 
-	private void scanXmlMappers(Configuration configuration) {
-		String resource = "mybatis/mappers/LogMapper.xml";
-		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(resource);
+        for (Class<?> mapperInterface : configuration.getMapperRegistry().getMappers()) {
+            bind((Class<Object>) mapperInterface).toInstance(Reflection.newProxy(mapperInterface,
+                    new MapperInterceptor(sqlSessionFactory, mapperInterface)));
+        }
 
-		XMLMapperBuilder xmlMapperBuilder = new XMLMapperBuilder(inputStream, configuration, resource,
-				configuration.getSqlFragments());
-		xmlMapperBuilder.parse();
-	}
+    }
 
-	private HikariDataSource getDataSource() {
-		HikariConfig config = new HikariConfig();
-		config.setJdbcUrl("jdbc:mysql://localhost/core?characterEncoding=utf8&serverTimezone=GMT%2B8");
-		config.setUsername("dev");
-		config.setPassword("123456");
-		return new HikariDataSource(config);
-	}
+    private Configuration createMyBatisConfiguration(DataSource dataSource) {
+        Environment environment = new Environment("dev", new JdbcTransactionFactory(), dataSource);
+        Configuration configuration = new Configuration(environment);
+        configuration.setMapUnderscoreToCamelCase(true);
+        return configuration;
+    }
 
-	private static class MapperInterceptor<T> implements InvocationHandler {
+    private void scanXmlMappers(Configuration configuration) {
+        String resource = "mybatis/mappers/LogMapper.xml";
+        InputStream inputStream =
+                Thread.currentThread().getContextClassLoader().getResourceAsStream(resource);
 
-		private SqlSessionFactory sqlSessionFactory;
+        XMLMapperBuilder xmlMapperBuilder = new XMLMapperBuilder(inputStream, configuration,
+                resource, configuration.getSqlFragments());
+        xmlMapperBuilder.parse();
+    }
 
-		private Class<T> mapperInterface;
+    private HikariDataSource getDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(
+                "jdbc:mysql://localhost/core?characterEncoding=utf8&serverTimezone=GMT%2B8");
+        config.setUsername("dev");
+        config.setPassword("123456");
+        return new HikariDataSource(config);
+    }
 
-		public MapperInterceptor(SqlSessionFactory sqlSessionFactory, Class<T> mapperInterface) {
-			this.sqlSessionFactory = Objects.requireNonNull(sqlSessionFactory, "sqlSessionFactory");
-			this.mapperInterface = Objects.requireNonNull(mapperInterface, "mapperInterface");
-		}
+    private static class MapperInterceptor<T> implements InvocationHandler {
 
-		@Override
-		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-			try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-				T instance = sqlSession.getMapper(mapperInterface);
-				try {
-					Object result = method.invoke(instance, args);
-					sqlSession.commit();
-					return result;
-				} catch (Exception e) {
-					sqlSession.rollback();
-					throw e;
-				}
-			}
-		}
-	}
+        private SqlSessionFactory sqlSessionFactory;
+
+        private Class<T> mapperInterface;
+
+        public MapperInterceptor(SqlSessionFactory sqlSessionFactory, Class<T> mapperInterface) {
+            this.sqlSessionFactory = Objects.requireNonNull(sqlSessionFactory, "sqlSessionFactory");
+            this.mapperInterface = Objects.requireNonNull(mapperInterface, "mapperInterface");
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+                T instance = sqlSession.getMapper(mapperInterface);
+                try {
+                    Object result = method.invoke(instance, args);
+                    sqlSession.commit();
+                    return result;
+                } catch (Exception e) {
+                    sqlSession.rollback();
+                    throw e;
+                }
+            }
+        }
+    }
 
 }
